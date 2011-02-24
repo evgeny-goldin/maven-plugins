bec8dcf422a15bf5bf3f5e3f036d9cda00bd9dde
Evgeny Goldin <evgenyg@gmail.com>
2011-02-24 08:04:30 +0200

    Issues fixed:
    pl-326: Provide a number of commands instead of a single command
    pl-328: Provide a way to echo current directory
    pl-329: Add <verbose>
    pl-330: Provide an option to echo commands executed

--- maven-sshexec-plugin/src/main/groovy/com/goldin/plugins/sshexec/SshexecMojo.groovy
+++ maven-sshexec-plugin/src/main/groovy/com/goldin/plugins/sshexec/SshexecMojo.groovy
@@ -6,7 +6,7 @@ import org.jfrog.maven.annomojo.annotations.MojoGoal
 import org.jfrog.maven.annomojo.annotations.MojoParameter
 import org.jfrog.maven.annomojo.annotations.MojoPhase
 
-/**
+ /**
  * MOJO that executes "sshexec"
  *
  * See
@@ -23,11 +23,37 @@ public class SshexecMojo extends BaseGroovyMojo
     @MojoParameter( required = true )
     public String location
 
-    /**
-     * The command to run on the remote host
-     */
-    @MojoParameter( required = true )
+    @MojoParameter( required = false )
+    public boolean verbose = false
+
+    @MojoParameter( required = false )
+    public boolean echoPwd = true
+
+    @MojoParameter( required = false )
+    public boolean echoCommands = false
+
+    @MojoParameter( required = false )
     public String command
+
+    @MojoParameter( required = false )
+    public String[] commands
+
+
+    /**
+     * Retrieves all execution commands
+     * @return all execution commands
+     */
+    private String[] commands ()
+    {
+        String[] commands = GCommons.general().array( this.commands, this.command, String )
+        commands          = commands*.split( /,|;/ ).flatten()*.trim().findAll{ it }.
+                            collect { String command -> [( echoCommands ? "echo Running [$command]:" : '' ), command ] }.
+                            flatten()
+
+        ([ echoPwd ? 'echo Current directory is [`pwd`]' : '' ] + commands ).
+            flatten().findAll{ it }
+    }
+
 
     public SshexecMojo ()
     {
@@ -37,24 +63,25 @@ public class SshexecMojo extends BaseGroovyMojo
     @Override
     void doExecute()
     {
-        assert location, "<location> is not specified"
-        assert command,  "<command> is not specified"
-
         Map<String, String> data      = GCommons.net().parseNetworkPath( location )
-        String              username  = data.username
-        String              password  = data.password
-        String              host      = data.host
-        String              directory = data.directory
+        String              username  = data[ 'username' ]
+        String              password  = data[ 'password' ]
+        String              host      = data[ 'host' ]
+        String              directory = data[ 'directory' ]
 
-        log.info( "Running sshexec [$command] on [$host:$directory]" )
+        long   t        = System.currentTimeMillis()
+        String command  = [ "cd $directory", *commands() ].join( '; ' )
 
-        new AntBuilder().sshexec( command     : "cd $directory $command",
+        log.info( "==> Running sshexec [$command] on [$host:$directory]" )
+
+        new AntBuilder().sshexec( command     : command,
                                   host        : host,
                                   username    : username,
                                   password    : password,
                                   trust       : true,
+                                  verbose     : verbose,
                                   failonerror : true )
 
-        log.info( "Sshexec [$command] run on [$host:$directory]" )
+        log.info( "==> Sshexec [$command] run on [$host:$directory] ([${ System.currentTimeMillis() - t }] ms)" )
     }
 }
