56c34c634b665373bcdfc43a90eadc22215221e1
Evgeny Goldin <evgenyg@gmail.com>
2011-05-07 23:53:32 +0300

    AboutMojo - almost done

--- maven-about-plugin/src/main/groovy/com/goldin/plugins/about/AboutMojo.groovy
+++ maven-about-plugin/src/main/groovy/com/goldin/plugins/about/AboutMojo.groovy
@@ -5,6 +5,8 @@ import org.jfrog.maven.annomojo.annotations.MojoPhase
 import com.goldin.plugins.common.BaseGroovyMojo
 import org.jfrog.maven.annomojo.annotations.MojoParameter
 import com.goldin.gcommons.GCommons
+import java.text.SimpleDateFormat
+import com.goldin.plugins.common.GMojoUtils
 
 /**
  * Updates files specified with "about" build metadata
@@ -13,9 +15,14 @@ import com.goldin.gcommons.GCommons
 @MojoPhase( 'package' )
 class AboutMojo extends BaseGroovyMojo
 {
+    @MojoParameter
+    public String scm = 'svn'
 
     @MojoParameter
-    public String scm
+    public boolean dumpEnv = true
+
+    @MojoParameter
+    public boolean dumpSystem = true
 
     @MojoParameter
     public File directory
@@ -39,37 +46,118 @@ class AboutMojo extends BaseGroovyMojo
         this.include   = file.name
     }
 
+    String exec ( String command )                { command.execute().text }
+    String find ( String prefix, String command ) { find( prefix, exec( command ).readLines()) }
+    String find ( String prefix, List<String> l ) { l.find{ it.startsWith( prefix ) }.replace( prefix, '' ).trim() }
+    String sort ( Map<String,String> map )        { def maxKey = map.keySet().collect { it.size() }.max()
+                                                    map.sort().
+                                                        collect { String key, String value ->
+                                                                  "[$key]".padRight( maxKey + 3 ) + ":[$value]" }.
+                                                        join( '\n' )
+    }
 
-    /**
-     * Splits the String specified.
-     * @param s String to split
-     * @return String specified split to tokens
-     */
-    def split( String s ) { s ? s.split( /,/ ).toList()*.trim().findAll{ it } : null }
-
-
-    def content()
+    String generalContent()
     {
-        'aaaaaaaaaaa'
+        def env   = System.getenv()
+        def props = System.properties
+
+        """
+        |===============================================================================
+        | Maven Info
+        |===============================================================================
+        | M2_HOME       : [${ env[ 'M2_HOME' ]}]
+        | Version       : [${ GMojoUtils.mavenVersion() }]
+        | Basedir       : [$basedir.canonicalPath]
+        | Project       : [$project]
+        | Name          : [$project.name]
+        | Coordinates   : [$project.groupId:$project.artifactId:$project.version]
+        |===============================================================================
+        | Build Info
+        |===============================================================================
+        | Host          : [${ env[ 'COMPUTERNAME' ] ?: env[ 'HOSTNAME' ] }]
+        | Time          : [${ new SimpleDateFormat( "dd MMM, EEEE, yyyy, HH:mm:ss (zzzzzz:'GMT'ZZZZZZ)", Locale.ENGLISH ).format( new Date()) }]
+        | User          : [${ props[ 'user.name' ] }]
+        | Directory     : [${ props[ 'user.dir'  ] }]
+        | Java          : [${ props[ 'java.version' ] }][${ props[ 'java.vm.vendor' ] }][${ props[ 'java.home' ] }][${ props[ 'java.vm.name' ] }]
+        | OS            : [${ props[ 'os.name' ] }][${ props[ 'os.arch' ] }][${ props[ 'os.version' ] }]
+        |===============================================================================""" +
+
+        ( dumpSystem ?
+
+        """
+        | System Properties
+        |===============================================================================
+        |${ sort( props ) }
+        |===============================================================================
+        """ : '' ) +
+
+        ( dumpEnv ?
+        
+        """
+        | Environment Variables
+        |===============================================================================
+        |${ sort( env ) }
+        |===============================================================================
+        """ : '' )
+    }
+
+    
+    String svnContent()
+    {
+        assert exec( 'svn --version' ), "Failed to run 'svn --version'"
+        def svnInfo = exec( "svn info ${basedir.canonicalPath}" ).readLines()
+        def commit  = exec( 'svn log -l 1' ).readLines()[ 1 ]
+
+        """
+        |===============================================================================
+        | SVN Info
+        |===============================================================================
+        | Repository    : [${ find( 'URL:',      svnInfo )}]
+        | Revision      : [${ find( 'Revision:', svnInfo )}]
+        | Last Commit   : [$commit]
+        | Commit Date   : [${ commit.split( '\\|' )[ 2 ].trim() }]
+        | Commit Author : [${ commit.split( '\\|' )[ 1 ].trim() }]
+        """ + generalContent()
+    }
+
+    
+    String gitContent()
+    {
+        assert exec( 'git --version' ), "Failed to run 'git --version'"
+        def gitLog = exec( 'git log -1' ).readLines()
+
+        """
+        |===============================================================================
+        | Git Info
+        |===============================================================================
+        | Repository    : [${ find( 'origin',      'git remote -v' )}]
+        | Branch        : [${ find( '# On branch', 'git status' )}]
+        | Last Commit   : [${ find( 'commit',      gitLog )}]
+        | Commit Date   : [${ find( 'Date:',       gitLog )}]
+        | Commit Author : [${ find( 'Author:',     gitLog )}]
+        """ + generalContent()
     }
 
 
     @Override
     void doExecute ()
     {
+        def split    = { String s -> ( s ? s.split( /,/ ).toList()*.trim().findAll{ it } : null ) }
         def files    = GCommons.file().files( directory, split( include ?: '*.jar' ), split( exclude ))
         def tempFile = new File( outputDirectory, "about-${project.groupId}-${project.artifactId}-${project.version}.txt" )
-        tempFile.write( content())
+        tempFile.write((( 'svn' == scm ) ? svnContent() : gitContent()).stripMargin().trim())
 
         for ( file in files )
         {
+            log.info( "Updating [$file.canonicalPath] .." )
+
             new AntBuilder().zip( destfile: file.canonicalPath,
                                   update  : true ){
                 zipfileset( file  : tempFile.canonicalPath,
                             prefix: 'META-INF' )
             }
 
-            log.info( "[$file.canonicalPath] updated" )
+            log.info( "Updating [$file.canonicalPath] - Done" )
         }
     }
 }
