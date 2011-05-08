474091beeefcb7d30e39393731d09539f4315a87
Evgeny Goldin <evgenyg@gmail.com>
2011-05-08 22:03:56 +0300

    AboutMojo - runs for all plugins, default directory set, svn/git verifications added, more Maven data added

--- maven-about-plugin/src/main/groovy/com/goldin/plugins/about/AboutMojo.groovy
+++ maven-about-plugin/src/main/groovy/com/goldin/plugins/about/AboutMojo.groovy
@@ -1,12 +1,11 @@
 package com.goldin.plugins.about
 
-import org.jfrog.maven.annomojo.annotations.MojoGoal
-import org.jfrog.maven.annomojo.annotations.MojoPhase
 import com.goldin.plugins.common.BaseGroovyMojo
-import org.jfrog.maven.annomojo.annotations.MojoParameter
-import com.goldin.gcommons.GCommons
 import java.text.SimpleDateFormat
-import com.goldin.plugins.common.GMojoUtils
+import org.jfrog.maven.annomojo.annotations.MojoGoal
+import org.jfrog.maven.annomojo.annotations.MojoParameter
+import org.jfrog.maven.annomojo.annotations.MojoPhase
+import static com.goldin.plugins.common.GMojoUtils.*
 
 /**
  * Updates files specified with "about" build metadata
@@ -16,28 +15,25 @@ import com.goldin.plugins.common.GMojoUtils
 class AboutMojo extends BaseGroovyMojo
 {
     @MojoParameter
-    public String scm         = 'svn'
+    public boolean dumpEnv    = false
 
     @MojoParameter
-    public boolean dumpEnv    = true
+    public boolean dumpSystem = false
 
     @MojoParameter
-    public boolean dumpSystem = true
-
-    @MojoParameter
-    public boolean dumpPaths  = true
+    public boolean dumpPaths  = false
 
     @MojoParameter
     public String endOfLine   = 'windows'
 
-    @MojoParameter
+    @MojoParameter ( defaultValue = '${project.build.directory}' )
     public File directory
 
     @MojoParameter
     public File file
 
     @MojoParameter
-    public String include
+    public String include = '*.jar'
     
     @MojoParameter
     public String exclude
@@ -45,41 +41,47 @@ class AboutMojo extends BaseGroovyMojo
 
     public void setFile ( File file )
     {
-        GCommons.verify().file( file )
+        verifyBean().file( file )
         assert file.parentFile, "File [$file.canonicalPath] has no parent directory"
 
         this.directory = file.parentFile
         this.include   = file.name
     }
 
-    String exec     ( String command )                { command.execute().text }
-    String padLines ( String s, int j )               { def lines = s.readLines()
-                                                        ( lines ? ( lines[ 0 ] +
-                                                                    (( lines.size() > 1 ) ? '\n' + lines[ 1 .. -1 ].collect { ( ' ' * j ) + it }.join( '\n' ) :
-                                                                                            '' )) :
-                                                                    '' )
+    
+    String padLines ( String s, int j )
+    {
+        def lines = s.readLines()
+        ( lines ? ( lines[ 0 ] + (( lines.size() > 1 ) ? '\n' + lines[ 1 .. -1 ].collect { ( ' ' * j ) + it }.join( '\n' ) :
+                                                         '' )) :
+                  '' )
     }
-    String find     ( String prefix, String command ) { find( prefix, exec( command ).readLines()) }
-    String find     ( String prefix, List<String> l ) { l.find{ it.startsWith( prefix ) }.replace( prefix, '' ).trim() }
-    String sort     ( Map<String,String> map )        { def maxKey = map.keySet().collect { it.size() }.max()
-                                                        map.sort().
-                                                            collect { String key, String value ->
-                                                                      "[$key]".padRight( maxKey + 3 ) + ":[$value]" }.
-                                                            join( '\n' )
+
+    String exec  ( String command )                { command.execute().text }
+    String find  ( String prefix, String command ) { find( prefix, exec( command ).readLines()) }
+    String find  ( String prefix, List<String> l ) { l.find{ it.startsWith( prefix ) }.replace( prefix, '' ).trim() }
+    String sort  ( Map<String,String> map )        { def maxKey = map.keySet()*.size().max()
+                                                     map.sort().
+                                                         collect { String key, String value ->
+                                                                   "[$key]".padRight( maxKey + 3 ) + ":[$value]" }.
+                                                         join( '\n' )
     }
 
     String generalContent()
     {
-        def env   = System.getenv()
-        def props = System.properties
+        def env    = System.getenv()
+        def props  = System.properties
+        def format = new SimpleDateFormat( "dd MMM, EEEE, yyyy, HH:mm:ss (zzzzzz:'GMT'ZZZZZZ)", Locale.ENGLISH )
 
         """
         |===============================================================================
         | Maven Info
         |===============================================================================
         | M2_HOME       : [${ env[ 'M2_HOME' ]}]
-        | Version       : [${ GMojoUtils.mavenVersion() }]
+        | MAVEN_OPTS    : [${ env[ 'MAVEN_OPTS' ]}]
+        | Version       : [${ mavenVersion() }]
         | Project       : [$project]
+        | Goals         : $session.goals
         | Basedir       : [$basedir.canonicalPath]
         | Name          : [$project.name]
         | Coordinates   : [$project.groupId:$project.artifactId:$project.version]
@@ -87,7 +89,8 @@ class AboutMojo extends BaseGroovyMojo
         | Build Info
         |===============================================================================
         | Host          : [${ env[ 'COMPUTERNAME' ] ?: env[ 'HOSTNAME' ] }]
-        | Time          : [${ new SimpleDateFormat( "dd MMM, EEEE, yyyy, HH:mm:ss (zzzzzz:'GMT'ZZZZZZ)", Locale.ENGLISH ).format( new Date()) }]
+        | Build Time    : Started         - [${ format.format( session.startTime ) }]
+        | Build Time    : "About" created - [${ format.format( new Date())         }]
         | User          : [${ props[ 'user.name' ] }]
         | Directory     : [${ props[ 'user.dir'  ] }]
         | Java          : [${ props[ 'java.version' ] }][${ props[ 'java.vm.vendor' ] }][${ props[ 'java.home' ] }][${ props[ 'java.vm.name' ] }]
@@ -115,6 +118,11 @@ class AboutMojo extends BaseGroovyMojo
     String svnContent()
     {
         assert exec( 'svn --version' ), "Failed to run 'svn --version'"
+        verifyBean().directory( new File( basedir, '.svn' ))
+        
+        def status  = exec( "svn status $basedir.canonicalPath" )
+        assert ! status.contains( 'is not a working copy' ), "[$basedir.canonicalPath] is not an SVN project"
+
         def svnInfo = exec( "svn info ${basedir.canonicalPath}" ).readLines()
         def commit  = exec( 'svn log -l 1' ).readLines()[ 1 ]
 
@@ -124,7 +132,7 @@ class AboutMojo extends BaseGroovyMojo
         |===============================================================================
         | Repository    : [${ find( 'URL:',      svnInfo )}]
         | Revision      : [${ find( 'Revision:', svnInfo )}]
-        | Status        : [${ padLines( exec( 'svn status' ), ' Status        : ['.size() ) }]
+        | Status        : [${ padLines( status, ' Status        : ['.size()) }]
         | Last Commit   : [$commit]
         | Commit Date   : [${ commit.split( '\\|' )[ 2 ].trim() }]
         | Commit Author : [${ commit.split( '\\|' )[ 1 ].trim() }]""" + generalContent()
@@ -134,15 +142,18 @@ class AboutMojo extends BaseGroovyMojo
     String gitContent()
     {
         assert exec( 'git --version' ), "Failed to run 'git --version'"
+        def status = exec( 'git status' )
+        assert ! status.contains( 'Not a git repository' ), "[$basedir.canonicalPath] is not a Git project"
+
         def gitLog = exec( 'git log -1' ).readLines()
 
         """
         |===============================================================================
         | Git Info
         |===============================================================================
-        | Repository    : [${ find( 'origin',      'git remote -v' )}]
+        | Repositories  : [${ padLines( exec( 'git remote -v' ), ' Repository    : ['.size()) }]
         | Branch        : [${ find( '# On branch', 'git status' )}]
-        | Status        : [${ padLines( exec( 'git status' ), ' Status        : ['.size() ) }]
+        | Status        : [${ padLines( status, ' Status        : ['.size() ) }]
         | Last Commit   : [${ find( 'commit',      gitLog )}]
         | Commit Date   : [${ find( 'Date:',       gitLog )}]
         | Commit Author : [${ find( 'Author:',     gitLog )}]""" + generalContent()
@@ -153,10 +164,11 @@ class AboutMojo extends BaseGroovyMojo
     void doExecute ()
     {
         def split    = { String s -> ( s ? s.split( /,/ ).toList()*.trim().findAll{ it } : null ) }
-        def files    = GCommons.file().files( directory, split( include ?: '*.jar' ), split( exclude ))
+        def files    = fileBean().files( directory, split( include ), split( exclude ))
         def tempFile = new File( outputDirectory, "about-${project.groupId}-${project.artifactId}-${project.version}.txt" )
-        def content  = (( 'svn' == scm ) ? svnContent() : gitContent()).stripMargin().trim().
-                       replaceAll( /\r?\n/, ( 'windows' == endOfLine ) ? '\r\n' : '\n' )
+        def content  = (( new File( basedir, '.svn' ).isDirectory() ? svnContent() : gitContent()).
+                        stripMargin().trim().replaceAll( /\r?\n/, ( 'windows' == endOfLine ) ? '\r\n' : '\n' ))
+        
         tempFile.write( content )
 
         for ( file in files )
