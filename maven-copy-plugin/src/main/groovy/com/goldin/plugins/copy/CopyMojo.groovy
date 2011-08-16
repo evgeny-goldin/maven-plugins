a2f433689a0d7a6061836b70cd017d9dfbebc119
Evgeny Goldin <evgenyg@gmail.com>
2011-08-16 18:17:49 +0300

    pl-451: Switch to TrueZip unpacking by default - undo for now, tests fails with NPE in TrueZip code

--- maven-copy-plugin/src/main/groovy/com/goldin/plugins/copy/CopyMojo.groovy
+++ maven-copy-plugin/src/main/groovy/com/goldin/plugins/copy/CopyMojo.groovy
@@ -631,7 +631,7 @@ class CopyMojo extends org.apache.maven.plugin.dependency.fromConfiguration.Copy
         File unpackDirectory = ( resource.replaces() || resource.filtering ) ? fileBean().tempDirectory() : destinationDirectory
 
         zipEntries ? fileBean().unpackZipEntries( sourceArchive, unpackDirectory, zipEntries, resource.preservePath, verbose ) :
-                     fileBean().unpack( sourceArchive, unpackDirectory, true )
+                     fileBean().unpack( sourceArchive, unpackDirectory )
 
         if ( ! unpackDirectory.is( destinationDirectory ))
         {
