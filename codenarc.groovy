6f071447899cd9f1f0d3ea5e0ca931a26911c4d3
Evgeny Goldin <evgenyg@gmail.com>
2011-10-03 15:23:27 +0200

    codenarc.txt => codenarc.groovy

---
+++ codenarc.groovy
@@ -0,0 +1,20 @@
+
+ruleset {
+
+    description 'GCommons CodeNarc RuleSet'
+
+    ruleset( "http://codenarc.sourceforge.net/StarterRuleSet-AllRulesByCategory.groovy.txt" ) {
+
+        DuplicateNumberLiteral   ( enabled : false )
+        DuplicateStringLiteral   ( enabled : false )
+        BracesForClass           ( enabled : false )
+        BracesForMethod          ( enabled : false )
+        BracesForIfElse          ( enabled : false )
+        BracesForForLoop         ( enabled : false )
+        BracesForTryCatchFinally ( enabled : false )
+        JavaIoPackageAccess      ( enabled : false )
+
+        LineLength               ( length  : 160   )
+        MethodName               ( regex   : /[a-z][\w\s'\(\)]*/ ) // Spock method names
+    }
+}
