ae9d6c51455588c5c775fddc19b06202c828382d
Evgeny Goldin <evgenyg@gmail.com>
2012-02-04 21:23:54 +0200

    <module>maven-ivy-plugin</module> - skeleton added

---
+++ maven-ivy-plugin/src/main/groovy/com/goldin/plugins/ivy/IvyMojo.groovy
@@ -0,0 +1,20 @@
+package com.goldin.plugins.ivy
+
+import com.goldin.plugins.common.BaseGroovyMojo
+import org.jfrog.maven.annomojo.annotations.MojoGoal
+import org.jfrog.maven.annomojo.annotations.MojoPhase
+
+
+/**
+ * Plugin that delegates artifacts resolving to Ivy.
+ */
+@MojoGoal ( 'generate' )
+@MojoPhase ( 'compile' )
+class IvyMojo extends BaseGroovyMojo
+{
+
+    @Override
+    void doExecute ()
+    {
+    }
+}
