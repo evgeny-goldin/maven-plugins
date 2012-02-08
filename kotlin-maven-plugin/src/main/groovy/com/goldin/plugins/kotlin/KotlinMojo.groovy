fff08701661987660054f974364cb09ccf06415e
Evgeny Goldin <evgenyg@gmail.com>
2012-02-08 23:52:28 +0200

    KotlinMojo - parameters added.

--- kotlin-maven-plugin/src/main/groovy/com/goldin/plugins/kotlin/KotlinMojo.groovy
+++ kotlin-maven-plugin/src/main/groovy/com/goldin/plugins/kotlin/KotlinMojo.groovy
@@ -1,10 +1,9 @@
 package com.goldin.plugins.kotlin
 
-import org.jetbrains.jet.compiler.CompileEnvironment
-import org.jetbrains.jet.compiler.CompileEnvironmentException
-
 import com.goldin.plugins.common.BaseGroovyMojo3
+import org.jetbrains.jet.buildtools.core.BytecodeCompiler
 import org.jfrog.maven.annomojo.annotations.MojoGoal
+import org.jfrog.maven.annomojo.annotations.MojoParameter
 import org.jfrog.maven.annomojo.annotations.MojoPhase
 
 
@@ -15,9 +14,49 @@ import org.jfrog.maven.annomojo.annotations.MojoPhase
 @MojoPhase ( 'compile' )
 class KotlinMojo extends BaseGroovyMojo3
 {
+    /**
+     * Source directories containing the sources to be compiled.
+     */
+    @MojoParameter( defaultValue = '${project.compileSourceRoots}' )
+    public List<String> sourceRoots
+
+    /**
+     * Project classpath.
+     */
+    @MojoParameter( defaultValue = '${project.compileClasspathElements}' )
+    public List<String> classpathElements
+
+    /**
+     * Directory for compiled classes.
+     */
+    @MojoParameter( defaultValue = '${project.build.outputDirectory}' )
+    public File output
+
+    @MojoParameter ( required = false )
+    public File jar
+
+    @MojoParameter ( required = false )
+    public File stdlib
+
+    @MojoParameter ( required = false )
+    public File src
+
+    @MojoParameter ( required = false )
+    public File module
+
+    @MojoParameter ( required = false )
+    public boolean includeRuntime = true
+
 
     @Override
     void doExecute ()
     {
+        final compiler = new BytecodeCompiler()
+
+        sourceRoots.each {
+            String sourceDir ->
+            log.info( "Compiling [$sourceDir] => [$output]" )
+            compiler.sourcesToDir( sourceDir, output, stdlib, classpathElements as String[] )
+        }
     }
 }
