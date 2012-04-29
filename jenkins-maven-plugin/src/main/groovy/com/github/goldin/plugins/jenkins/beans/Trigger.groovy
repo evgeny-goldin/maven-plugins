78ebde3e3a30a2aea6e9457ee8438ecebb2487ad
Evgeny Goldin <evgenyg@gmail.com>
2012-04-29 18:03:38 +0200

    pl-357: Support GitHub plugin - GitHub trigger added.

--- jenkins-maven-plugin/src/main/groovy/com/github/goldin/plugins/jenkins/beans/Trigger.groovy
+++ jenkins-maven-plugin/src/main/groovy/com/github/goldin/plugins/jenkins/beans/Trigger.groovy
@@ -6,11 +6,12 @@ package com.github.goldin.plugins.jenkins.beans
  */
 class Trigger
 {
-    static final Map TYPES = [ scm   : 'hudson.triggers.SCMTrigger',
-                               timer : 'hudson.triggers.TimerTrigger' ]
+    static final Map TYPES = [ scm    : 'hudson.triggers.SCMTrigger',
+                               timer  : 'hudson.triggers.TimerTrigger',
+                               github : 'com.cloudbees.jenkins.GitHubPushTrigger' ]
     String type
-    String description  // Description to be used (by normal humans) as comment
-    String expression   // Crontab expression
+    String description = ''  // Description to be used (by humans) as comment
+    String expression  = ''  // Trigger expression
 
     String getTriggerClass()
     {
