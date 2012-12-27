758a50e2b2c527ca01f0d5fd4cb98f8dd8d33cda
Evgeny Goldin <evgenyg@gmail.com>
2012-12-27 02:20:51 +0100

    Silencer Mojo starts working.

--- silencer-maven-plugin/src/main/groovy/com/github/goldin/plugins/silencer/SilencerLogger.groovy
+++ silencer-maven-plugin/src/main/groovy/com/github/goldin/plugins/silencer/SilencerLogger.groovy
@@ -1,11 +1,12 @@
 package com.github.goldin.plugins.silencer
 
 import org.apache.maven.plugin.logging.Log
+import org.codehaus.plexus.logging.Logger
 
 
 /**
  */
-class SilencerLogger implements Log
+class SilencerLogger implements Log, Logger
 {
     @Override
     boolean isDebugEnabled (){ false }
@@ -14,7 +15,7 @@ class SilencerLogger implements Log
     void debug ( CharSequence content ){}
 
     @Override
-    void debug ( CharSequence content , Throwable error ){}
+    void debug ( CharSequence content, Throwable error ){}
 
     @Override
     void debug ( Throwable error ){}
@@ -26,7 +27,7 @@ class SilencerLogger implements Log
     void info ( CharSequence content ){}
 
     @Override
-    void info ( CharSequence content , Throwable error ){}
+    void info ( CharSequence content, Throwable error ){}
 
     @Override
     void info ( Throwable error ){}
@@ -38,7 +39,7 @@ class SilencerLogger implements Log
     void warn ( CharSequence content ){}
 
     @Override
-    void warn ( CharSequence content , Throwable error ){}
+    void warn ( CharSequence content, Throwable error ){}
 
     @Override
     void warn ( Throwable error ){}
@@ -50,8 +51,53 @@ class SilencerLogger implements Log
     void error ( CharSequence content ){}
 
     @Override
-    void error ( CharSequence content , Throwable error ){}
+    void error ( CharSequence content, Throwable error ){}
 
     @Override
     void error ( Throwable error ){}
+
+    @Override
+    void debug ( String message ){}
+
+    @Override
+    void debug ( String message, Throwable throwable ){}
+
+    @Override
+    void info ( String message ){}
+
+    @Override
+    void info ( String message, Throwable throwable ){}
+
+    @Override
+    void warn ( String message ){}
+
+    @Override
+    void warn ( String message, Throwable throwable ){}
+
+    @Override
+    void error ( String message ){}
+
+    @Override
+    void error ( String message, Throwable throwable ){}
+
+    @Override
+    void fatalError ( String message ){}
+
+    @Override
+    void fatalError ( String message, Throwable throwable ){}
+
+    @Override
+    boolean isFatalErrorEnabled ( ){ return false }
+
+    @Override
+    int getThreshold ( ){ return 0 }
+
+    @Override
+    void setThreshold ( int threshold ){}
+
+    @Override
+    Logger getChildLogger ( String name ){ return null }
+
+    @Override
+    String getName (){ return null }
 }
--- silencer-maven-plugin/src/main/groovy/com/github/goldin/plugins/silencer/SilencerMojo.groovy
+++ silencer-maven-plugin/src/main/groovy/com/github/goldin/plugins/silencer/SilencerMojo.groovy
@@ -5,6 +5,8 @@ import org.apache.maven.lifecycle.internal.MojoExecutor
 import org.apache.maven.plugin.DefaultBuildPluginManager
 import org.apache.maven.plugins.annotations.LifecyclePhase
 import org.apache.maven.plugins.annotations.Mojo
+import java.lang.reflect.Field
+import java.lang.reflect.Modifier
 
 
 /**
@@ -13,11 +15,39 @@ import org.apache.maven.plugins.annotations.Mojo
 @Mojo( name = 'silence', defaultPhase = LifecyclePhase.VALIDATE, threadSafe = true )
 class SilencerMojo extends BaseGroovyMojo
 {
+
     @Override
     void doExecute ( )
     {
-        final executor = session.container.lookup( MojoExecutor )
-        (( DefaultBuildPluginManager ) executor.pluginManager ).mavenPluginManager =
-            new SilencerMavenPluginManager((( DefaultBuildPluginManager ) executor.pluginManager ).mavenPluginManager )
+        updateMavenPluginManager()
+        updateRepositoryLogger()
+    }
+
+    private void updateMavenPluginManager ( )
+    {
+        try
+        {
+            final executor = session.container.lookup( MojoExecutor )
+            (( DefaultBuildPluginManager ) executor.pluginManager ).mavenPluginManager =
+                new SilencerMavenPluginManager((( DefaultBuildPluginManager ) executor.pluginManager ).mavenPluginManager )
+        }
+        catch ( Throwable ignored ){}
+    }
+
+
+    private void updateRepositoryLogger ( )
+    {
+        try
+        {
+            final listener            = session.repositorySession.repositoryListener
+            final loggerField         = listener.class.getDeclaredField( 'logger' )
+            final modifiersField      = Field.class.getDeclaredField( 'modifiers' )
+            loggerField.accessible    = true
+            modifiersField.accessible = true
+
+            modifiersField.setInt( loggerField , loggerField.modifiers & ~Modifier.FINAL )
+            loggerField.set( listener , new SilencerLogger() )
+        }
+        catch ( Throwable ignored ){}
     }
 }
