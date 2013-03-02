88b6cd11399ad8e677b59ec4ba5f6ab6c10192bc
Evgeny Goldin <evgenyg@gmail.com>
2013-03-02 23:00:00 +0100

    Groovydoc cosmetics

--- groovydoc/src/main/groovy/groovydoc.groovy
+++ groovydoc/src/main/groovy/groovydoc.groovy
@@ -52,21 +52,19 @@ ant.groovydoc(
         use          : 'true',
         private      : 'false',
         header       : '<a href="http://evgeny-goldin.com/wiki/Maven-plugins">evgeny-goldin.com/wiki/Maven-plugins</a>',
-        footer       : """<a href="http://evgeny-goldin.com/">Evgeny Goldin</a>. GrovyDoc was generated at $time on $date.
+        footer       : """
+                       <a href="http://evgeny-goldin.com/">Evgeny Goldin</a>. GrovyDoc was generated at $time on $date.
                        <script type="text/javascript">
-
                           var _gaq = _gaq || [];
                           _gaq.push(['_setAccount', 'UA-7925869-2']);
                           _gaq.push(['_setDomainName', 'none']);
                           _gaq.push(['_setAllowLinker', true]);
                           _gaq.push(['_trackPageview']);
-
                           (function() {
                             var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
                             ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
                             var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
                           })();
-
                        </script>""".stripIndent())
     {
        link( packages :'org.apache.maven.artifact.',          href : "http://maven.apache.org/ref/$mavenVersion/maven-artifact/apidocs" )
