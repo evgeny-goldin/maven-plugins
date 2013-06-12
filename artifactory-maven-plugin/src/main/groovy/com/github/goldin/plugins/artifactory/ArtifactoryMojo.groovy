0557ca9760d00fa51abfddc3d54358b35bd4e9a9
Evgeny Goldin <evgenyg@gmail.com>
2013-06-13 00:58:49 +0200

    ArtifactoryMojo - properties file created.

--- artifactory-maven-plugin/src/main/groovy/com/github/goldin/plugins/artifactory/ArtifactoryMojo.groovy
+++ artifactory-maven-plugin/src/main/groovy/com/github/goldin/plugins/artifactory/ArtifactoryMojo.groovy
@@ -1,6 +1,7 @@
 package com.github.goldin.plugins.artifactory
 
 import static com.github.goldin.plugins.common.GMojoUtils.*
+import org.jfrog.build.api.BuildInfoConfigProperties
 import com.github.goldin.plugins.common.BaseGroovyMojo
 import org.apache.maven.AbstractMavenLifecycleParticipant
 import org.apache.maven.plugins.annotations.LifecyclePhase
@@ -17,9 +18,19 @@ class ArtifactoryMojo extends BaseGroovyMojo
     @Override
     void doExecute ()
     {
+        createProperties()
         final recorder = verifyBean().isInstance( container.lookup( AbstractMavenLifecycleParticipant ),
                                                   BuildInfoRecorderLifecycleParticipant )
         recorder.afterSessionStart( session )
         recorder.afterProjectsRead( session )
+    }
+
+
+    private void createProperties()
+    {
+        final propertiesFile    = fileBean().tempFile( '.properties' )
+        final propertiesContent = this.class.classLoader.getResourceAsStream( 'buildInfo.properties' ).getText( 'UTF-8' )
+        propertiesFile.setText( propertiesContent, 'UTF-8' )
+        System.setProperty( BuildInfoConfigProperties.PROP_PROPS_FILE, propertiesFile.canonicalPath )
     }
 }
